b2060ff24290565b3c8963ff583322a68fb7923b
finddme <yein4452@gmail.com>
2026-06-13 23:03:04 +0900

    Adjust stage basis to 1580x1324

--- public/js/lab-archive.js
+++ public/js/lab-archive.js
@@ -12,8 +12,8 @@
   var desktopMode = window.matchMedia("(min-width: 821px)");
   var connectorKeys = ["nlp", "all", "multi", "ling"];
 
-  var STAGE_W = 1550;
-  var STAGE_H = 1299;
+  var STAGE_W = 1580;
+  var STAGE_H = 1324;
 
   function updateStageScale() {
     if (!stage || !hero) {
