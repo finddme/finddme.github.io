fcd18f59acc184251ed9d970347362a8fb35af37
finddme <yein4452@gmail.com>
2026-06-13 23:00:03 +0900

    Set stage to 1500x1257 and move grid to full-viewport background

--- public/js/lab-archive.js
+++ public/js/lab-archive.js
@@ -12,8 +12,8 @@
   var desktopMode = window.matchMedia("(min-width: 821px)");
   var connectorKeys = ["nlp", "all", "multi", "ling"];
 
-  var STAGE_W = 1358;
-  var STAGE_H = 1138;
+  var STAGE_W = 1500;
+  var STAGE_H = 1257;
 
   function updateStageScale() {
     if (!stage || !hero) {
